583951b4a3ac8beeebe3ef4c0abdb503c340e7f0
Traister101 <66151907+Traister101@users.noreply.github.com>
2025-09-17 15:32:46 -0700

    FluidHeatProvider

---
+++ src/main/java/mod/traister101/datagenutils/data/tfc/FluidHeatProvider.java
@@ -0,0 +1,36 @@
+package mod.traister101.datagenutils.data.tfc;
+
+import net.dries007.tfc.util.data.*;
+
+import net.minecraft.core.HolderLookup.Provider;
+import net.minecraft.data.PackOutput;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.material.Fluid;
+
+import java.util.concurrent.CompletableFuture;
+
+public abstract class FluidHeatProvider extends DataManagerProvider<FluidHeat> {
+
+	public static final float HEAT_CAPACITY = 0.003f;
+
+	protected FluidHeatProvider(final PackOutput output, final String modid, final CompletableFuture<Provider> lookup) {
+		super(FluidHeat.MANAGER, output, modid, lookup);
+	}
+
+	protected static FluidHeat fluidHeat(final Fluid fluid, final float baseHeatCapacity, final float meltTemperature) {
+		return new FluidHeat(fluid, meltTemperature, HEAT_CAPACITY / baseHeatCapacity);
+	}
+
+	protected final void add(final String name, final Fluid fluid, final float baseHeatCapacity, final float meltTemperature) {
+		add(name, fluidHeat(fluid, baseHeatCapacity, meltTemperature));
+	}
+
+	protected final void add(final ResourceLocation name, final Fluid fluid, final float baseHeatCapacity, final float meltTemperature) {
+		add(name, fluidHeat(fluid, baseHeatCapacity, meltTemperature));
+	}
+
+	protected final void add(final DataManager.Reference<FluidHeat> reference, final Fluid fluid, final float baseHeatCapacity,
+			final float meltTemperature) {
+		add(reference, fluidHeat(fluid, baseHeatCapacity, meltTemperature));
+	}
+}
